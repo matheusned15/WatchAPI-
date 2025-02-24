12a48c584e7b0ccdc45be6e14ac982db81d3cb9f
matheusned15 <matheusned15@gmail.com>
2025-02-23 21:10:05 -0300

    First commit

---
+++ src/main/java/com/example/WatchAPI/WatchApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.WatchAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WatchAPI/WatchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WatchAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
